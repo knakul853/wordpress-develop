30f3ccbc3bd9bccd28faffa0897b5027c3b989e7
Nakul Bharti <knakul853@gmail.com>
2026-07-22 17:24:42 +0530

    Add benign arithmetic helpers

---
+++ src/neo-benign-helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Simple arithmetic helpers (benign, no security relevance).
+ */
+function neo_benign_add( $a, $b ) {
+    return $a + $b;
+}
+function neo_benign_multiply( $a, $b ) {
+    return $a * $b;
+}
